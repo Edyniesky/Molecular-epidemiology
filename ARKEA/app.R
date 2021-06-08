4d05f4b3aefa9bc2516f8d55f930768092c6e56f
Edyniesky Ferrer <63436389+Edyniesky@users.noreply.github.com>
2021-06-08 12:43:34 -0300

    update

--- ARKEA/app.R
+++ ARKEA/app.R
@@ -185,7 +185,8 @@ report2 <- left_join(report1, Cent, by = "Municipio")
 
 report2i <- report2 %>% 
   select(Point_x, Point_y, lineage, n) %>% 
-  spread(lineage, n, fill = 0) %>% 
+  #spread(lineage, n, fill = 0) %>% 
+  mutate(lineage = factor(lineage)) %>% 
   ungroup() 
 
 col.name1 <- report2i %>% 
@@ -196,7 +197,58 @@ estPE <- st_read("estadoPE.shp", quiet = TRUE)
 munPE <- st_read("municipioPE1.shp", quiet = TRUE)
 
 
+icons <- awesomeIcons(
+  icon = 'ios-close',
+  iconColor = 'black',
+  library = 'ion',
+  markerColor = getColor(breweries91)
+)
 
+#Generate the javascript
+
+jsscript3 <-
+  paste0(
+    "function(cluster) {
+const groups = [", paste("'", levels(report2i$lineage), "'", sep = "", collapse = ","), "];
+const colors = {
+groups: [", paste( "'", joliepalette,"'", sep = "", collapse = ","), "],
+center:'#ddd',
+text:'black'
+};
+const markers= cluster.getAllChildMarkers();
+
+const proportions= groups.map(group => markers.filter(marker => marker.options.group === group).length / markers.length);
+function sum(arr, first= 0, last) {
+return arr.slice(first, last).reduce((total, curr) => total+curr, 0);
+}
+const cumulativeProportions= proportions.map((val, i, arr) => sum(arr, 0, i+1));
+cumulativeProportions.unshift(0);
+
+const width = 2*Math.sqrt(markers.length);
+const radius= 15+width/2;
+
+const arcs= cumulativeProportions.map((prop, i) => { return {
+x   :  radius*Math.sin(2*Math.PI*prop),
+y   : -radius*Math.cos(2*Math.PI*prop),
+long: proportions[i-1] >.5 ? 1 : 0
+}});
+const paths= proportions.map((prop, i) => {
+if (prop === 0) return '';
+else if (prop === 1) return `<circle cx='0' cy='0' r='${radius}' fill='none' stroke='${colors.groups[i]}' stroke-width='${width}' stroke-alignment='center' stroke-linecap='butt' />`;
+else return `<path d='M ${arcs[i].x} ${arcs[i].y} A ${radius} ${radius} 0 ${arcs[i+1].long} 1 ${arcs[i+1].x} ${arcs[i+1].y}' fill='none' stroke='${colors.groups[i]}' stroke-width='${width}' stroke-alignment='center' stroke-linecap='butt' />`
+});
+
+return new L.DivIcon({
+html: `
+<svg width='60' height='60' viewBox='-30 -30 60 60' style='width: 60px; height: 60px; position: relative; top: -24px; left: -24px;' >
+<circle cx='0' cy='0' r='20' stroke='none' fill='${colors.center}' />
+<text x='0' y='0' dominant-baseline='central' text-anchor='middle' fill='${colors.text}' font-size='15'>${markers.length}</text>
+${paths.join('')}
+</svg>
+`,
+className: 'marker-cluster'
+});
+}")
 
 
 
@@ -421,9 +473,16 @@ server <- function(input, output) {
     
     output$map2i <- renderLeaflet({
       
-      colors1 <- c("#ADFF2F", "#8B8B83", "#FFA500", "#1E90FF", "#FFD700", "#EE2C2C", "#8470FF", "#006400")
       
-      map2 <- leaflet() %>% 
+      
+      joliepalette <- c("#ADFF2F", "#8B8B83", "#FFA500", "#1E90FF", "#FFD700", "#EE2C2C", "#8470FF", 
+                      "#006400")[1:nlevels(report2i$lineage)]
+      
+      getColor <- function(report2i) {joliepalette[report2i$lineage]}
+
+      #colors1 <- c("#ADFF2F", "#8B8B83", "#FFA500", "#1E90FF", "#FFD700", "#EE2C2C", "#8470FF", "#006400")
+      
+      map2 <- leaflet(width = "100%", height = "700px")  %>% 
         addTiles() %>% 
         addMiniMap(tiles = providers$Esri.WorldStreetMap,
                    toggleDisplay = TRUE,
@@ -459,15 +518,30 @@ server <- function(input, output) {
         addEasyButton(easyButton(
           icon = "fa-crosshairs", title = "Localize-me",
           onClick = JS("function(btn, map){ map.locate({setView: true}); }"))) %>% 
-        leaflet.minicharts::addMinicharts(report2i$Point_x, 
-                      report2i$Point_y, 
-                      type = "pie", 
-                      chartdata = col.name1,
-                      colorPalette = colors1, 
-                      opacity = 0.8,
-                      width = 45, 
-                      height = 30,
-                      legend = TRUE)  
+        #leaflet.minicharts::addMinicharts(report2i$Point_x, 
+                      #report2i$Point_y, 
+                     # type = "pie", 
+                      #chartdata = col.name1,
+                     # colorPalette = colors1, 
+                     # opacity = 0.8,
+                    #  width = 45, 
+                    #  height = 30,
+                  #    legend = TRUE)
+        addAwesomeMarkers(data = report2i,
+                          lng = report2i$Point_x,
+                          lat = report2i$Point_y,
+                          group = ~lineage,
+                          icon = icons,
+                          #label = ~lineage,
+                          clusterOptions = markerClusterOptions(
+                            iconCreateFunction =
+                              JS(jsscript3)),
+                          popup = paste0(
+                            "<strong>Município: </strong>", report2i$Municipio, "<br>",
+                            "<strong>Linhagens Pongo: </strong>", report2i$lineage,"<br>",
+                            "<strong>Número de sequências: </strong>",report2i$n, "<br>"
+                            
+                          ))
         
     })
     
